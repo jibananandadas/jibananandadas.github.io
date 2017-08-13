771986c9f9e807bde0f983be7ba42a6ddad2bc95
utsargo <royutsargo@gmail.com>
2017-08-14 02:29:33 +0600

    Update Book

---
+++ book/_book/publish.sh
@@ -0,0 +1,37 @@
+# install the plugins and build the static site
+gitbook install && gitbook build
+
+# Generate a PDF file
+gitbook pdf ./ ./mybook.pdf
+
+# Generate an ePub file
+gitbook epub ./ ./mybook.epub
+
+# Generate a Mobi file
+gitbook mobi ./ ./mybook.mobi
+
+cd ../
+# checkout to the gh-pages branch
+git checkout gh-pages
+
+# pull the latest updates
+git pull origin gh-pages --rebase
+
+# copy the static site files into the current directory.
+cp -R _book/* .
+
+# remove 'node_modules' and '_book' directory
+git clean -fx node_modules
+git clean -fx _book
+
+# add all files
+git add .
+
+# commit
+git commit -a -m "Update Book"
+
+# push to the origin
+git push origin gh-pages
+
+# checkout to the master branch
+git checkout master
